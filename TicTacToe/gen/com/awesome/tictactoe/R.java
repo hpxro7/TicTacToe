87f7480e28043198b50c0c8125af53819d567770
JTeo <jteosw@gmail.com>
2012-12-01 00:15:30 -0800

    renamed o picture

--- TicTacToe/gen/com/awesome/tictactoe/R.java
+++ TicTacToe/gen/com/awesome/tictactoe/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int o=0x7f020001;
+        public static final int x=0x7f020002;
     }
     public static final class id {
         public static final int menu_settings=0x7f070000;
