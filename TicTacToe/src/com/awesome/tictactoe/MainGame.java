d8a53696612014384cfc5142f23b32598e047468
shermpay <shermpay@uw.edu>
2012-12-01 00:44:44 -0800

    Test Sherman

--- TicTacToe/src/com/awesome/tictactoe/MainGame.java
+++ TicTacToe/src/com/awesome/tictactoe/MainGame.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainGame extends Activity {
-
+	// Thanks
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
